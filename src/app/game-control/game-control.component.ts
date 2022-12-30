1477d88550568c0ff3bea294f8e155a3c3ec7180
Justin Hornsby <justin.hornsby@entelect.co.za>
2022-12-30 13:23:49 +0200

    install bootstrap and implement start button

---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { VirtualTimeScheduler } from 'rxjs';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit{
+  @Output() intervalFired = new EventEmitter<number>();
+  interval:any;
+  lastNumber =0;
+
+  ngOnInit() {
+
+  }
+
+  onStartGame() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber+1);
+      this.lastNumber++;
+    },1000);
+  }
+}
